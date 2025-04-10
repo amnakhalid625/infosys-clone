65d16cfc44d67b9090af2a281539c1bc9bc13b78
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-10 21:03:43 +0500

    infosys_website_clone

--- src/pages/purpose/Purpose.jsx
+++ src/pages/purpose/Purpose.jsx
@@ -4,7 +4,7 @@ import './Purpose.css';
 const Purpose = () => {
   return (
     <section className="purpose-section">
-      <div className="purpose-container">
+      <div className="purpose-container ">
         <h2 className="purpose-heading">Our Purpose:</h2>
         <p className="purpose-text">
           To amplify human potential and create the next opportunity for people, businesses and communities
