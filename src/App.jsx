f05b0fed217767e5d8827867b2c0709fbb6d9570
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-11 00:38:50 +0500

    infosys_website_clone

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Capability from './pages/capabilities/Capability'
 import Model from './pages/operating-model/Model'
 import Talent from './pages/talent/Talent'
 import Next from './pages/next/Next'
-// import OffCanvas from './pages/offCanva/Canva'
 import Bell from './pages/bell/Bell';
 import Purpose from './pages/purpose/Purpose'
 
