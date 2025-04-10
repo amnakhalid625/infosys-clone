409ee7f5caea39b74e0a557a298e7db9d3c30362
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-10 20:24:10 +0500

    infosys_website_clone

--- src/pages/Bell/Bell.jsx
+++ src/pages/Bell/Bell.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './Bell.css';
 
-const bell = () => {
+const Bell = () => {
   return (
-  <>
-  <section className='container bell-section'>
-
-
-
-
-  </section>
-  
-  
-  </>
-  )
+    <section className='bell-section'>
+      <div className='bell-content'>
+        <h4 className='bell-heading'>Ring the Sound of Opportunity,</h4>
+        <h5 className='bell-subheading'>Move 1,000+1,000 Lives Forward.</h5>
+        <button className='bell-button'>READ MORE</button>
+      </div>
+    </section>
+  );
 }
 
-export default bell
+export default Bell;
