c02fb3b3296cafa9105e055f6380b884a3eded56
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-10 22:28:43 +0500

    infosys_website_clone

--- src/pages/capabilities/Capability.jsx
+++ src/pages/capabilities/Capability.jsx
@@ -1,65 +1,33 @@
-import React from 'react'
-import './capability.css'
-// import butterfly from '../../assets/butterfly.png'
+import React from 'react';
+import './capability.css';
 
 const Capability = () => {
     return (
-        <>
-            <div className='capability   background-buterfly '>
-            <div className="col-lg-6 col-sm-12 col-12 mx-5 ">
-                            <div>
-                                <h1>Digital Core Capabilities</h1>
-                                <p>Build vital capabilities to deliver digital outcomes.</p>
-                                <button>Explore</button>
-                                <div className='case-parent'>
-
-                                    <div className='case1'>
-                                        <p>case studies</p>
-                                        <h3>Pfizer's journey towards increased productivity, powered by AI</h3>
-                                        <p>view</p>
-
-                                    </div>
-                                    <div className='case2'>
-                                        <h3>A government that contributes to the joy of parenting</h3>
-                                        <p>view</p>
-                                    </div>
-
-                                </div>
+        <section className='capability-section'>
+            <div className='capability-container background-butterfly'>
+                <div className="content-wrapper">
+                    <div className="main-content">
+                        <h1>Digital Core Capabilities</h1>
+                        <p>Build vital capabilities to deliver digital outcomes.</p>
+                        <button className="explore-btn">Explore</button>
+                        
+                        <div className='case-studies'>
+                            <div className='case-study case1'>
+                                <p className="case-label">case studies</p>
+                                <h3>Pfizer's journey towards increased productivity, powered by AI</h3>
+                                <p className="view-link">view</p>
+                            </div>
+                            
+                            <div className='case-study case2'>
+                                <h3>A government that contributes to the joy of parenting</h3>
+                                <p className="view-link">view</p>
                             </div>
                         </div>
-                {/* <div className="capability ">
-                    <div className="row d-flex justify-content-start align-items-center">
-                        <div className="col-lg-6 col-sm-12 col-12">
-                            <div>
-                                <h1>Digital Core Capabilities</h1>
-                                <p>Build vital capabilities to deliver digital outcomes.</p>
-                                <button>Explore</button>
-                                <div className='case-parent'>
-
-                                    <div className='case1'>
-                                        <p>case studies</p>
-                                        <h3>Pfizer's journey towards increased productivity, powered by AI</h3>
-                                        <p>view</p>
-
-                                    </div>
-                                    <div className='case2'>
-                                        <h3>A government that contributes to the joy of parenting</h3>
-                                        <p>view</p>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                        {/* <div className="col-lg-6 col-sm-12 col-12 butterflyImg">
-                            <img src={butterfly} alt="" className='img-fluid' />
-                        </div> */}
-
-                    {/* </div> */}
-                {/* </div> */} 
-
+                    </div>
+                </div>
             </div>
-        </>
+        </section>
     )
 }
 
-export default Capability
+export default Capability;
